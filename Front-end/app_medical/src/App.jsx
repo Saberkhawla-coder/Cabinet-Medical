f52b71bc35d80b6f4b63b56d128c18f88d58bb49
Saberkhawla-coder <khawla.saber06@gmail.com>
2025-12-17 10:47:39 +0100

    Add routes Link Front end code

--- Front-end/app_medical/src/App.jsx
+++ Front-end/app_medical/src/App.jsx
@@ -1,15 +1,26 @@
-import './App.css'
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import {Routes, Route } from "react-router-dom";
+import Home from "./Components/pages/Home/Home";
+import About from "./Components/pages/About/About";
+import Contact from "./Components/pages/Contact/Contact";
+import Login from "./Components/Auth/Login";
+import Register from "./Components/Auth/Register";
 
 function App() {
-
   return (
     <>
-      <div>
-       
-      </div>
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
