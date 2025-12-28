20ffcec79484faaef9d35fec5373cf08d5a70ba0
Saberkhawla-coder <khawla.saber06@gmail.com>
2025-12-28 07:52:12 +0100

    I changed some styles in the About page.

--- Front-end/app_medical/src/Components/pages/About/About.jsx
+++ Front-end/app_medical/src/Components/pages/About/About.jsx
@@ -1,31 +1,26 @@
 import React from 'react'
-import Sante from '/public/images/Sante.jpg'
 import { Link } from 'react-router-dom'
 import SectionAbout from './SectionAbout'
 
 function About() {
   return (
     <>
-    <div className="relative w-full h-[400px] overflow-hidden">
+    <div className="relative w-full h-[70vh] flex justify-center items-center">
       
-      <img
-        src={Sante}
-        alt="About us background"
-        className="w-full h-full object-cover"
-      />
-
-      <div className="absolute inset-0 bg-black/85 flex flex-col items-center justify-center text-center">
+     <div className="absolute inset-0 bg-[url('/images/Healthy/bg.jpg')] bg-cover bg-center opacity-30"></div>
+        <div className="absolute inset-0 bg-sky-200/30 backdrop-blur-sm"></div>
+      <div className="relative max-w-5xl mx-auto text-center px-6">
         
-        <h1 className="text-5xl md:text-6xl font-bold text-white mb-4">
+        <h1 className="text-4xl md:text-5xl font-bold text-green-900 mb-4">
           About Us
         </h1>
 
-        <div className="flex items-center gap-2 text-white/80 text-sm">
+        <div className="flex items-center justify-center gap-2 text-gray-700 text-sm">
           <Link to="/" className="hover:text-[#4DA3FF] transition">
             Home
           </Link>
           <span>/</span>
-          <Link to="/about" className="text-[#4DA3FF] font-medium">
+          <Link to="/about" className="font-medium">
             About Us
           </Link>
         </div>
