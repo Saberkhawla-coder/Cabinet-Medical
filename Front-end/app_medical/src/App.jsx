51819778b1062065aaed9b267859bc5c6932bd68
Saberkhawla-coder <khawla.saber06@gmail.com>
2025-12-28 08:21:49 +0100

    I added logic so that some routes display the Navbar and others display the Sidebar.

--- Front-end/app_medical/src/App.jsx
+++ Front-end/app_medical/src/App.jsx
@@ -1,26 +1,76 @@
 import "./App.css";
-import Navbar from "./Components/Navbar";
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+
 import Home from "./Components/pages/Home/Home";
 import About from "./Components/pages/About/About";
 import Contact from "./Components/pages/Contact/Contact";
 import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
 
+import ChatPage from "./Components/ConnectedPatient/Chat/ChatPage";
+
+import PublicLayout from "./Components/layouts/PublicLayout";
+import AdminLayout from "./Components/layouts/AdminLayout";
+import AdminDashboard from "./Components/Dashboard/Admin/AdminDash/AdminDashboard";
+import PatientsDashboard from "./Components/Dashboard/Admin/Patients/PatientsDashboard";
+import MedcineDashboard from "./Components/Dashboard/Admin/Medcine/MedcineDashboard";
+import ContactDashoard from "./Components/Dashboard/Admin/ContactHis/ContactDashoard";
+import ProtectedRoute from "./Components/layouts/ProtectedRoute";
+import DoctorDashboard from "./Components/Dashboard/Doctor/DoctorDash/DoctorDashboard";
+import Rdv from "./Components/Dashboard/Admin/RDV/RdvCard";
+import RdvDashboard from "./Components/Dashboard/Admin/RDV/RdvDashboard";
+import { Toaster } from 'sonner';
+import BookAppointmentPage from "./Components/ConnectedPatient/BookAppointment/BookAppointmentPage";
+import AppointmentsPage from "./Components/ConnectedPatient/Appointments/AppointmentsPage";
+import HealthTipsPage from "./Components/ConnectedPatient/Health/HealthTipsPage";
 function App() {
+   
+
   return (
     <>
-      <Navbar />
-      <Routes>
+    <Toaster richColors  toastOptions={{
+    duration: 4000,          
+    style: {
+      borderRadius: '12px',  
+      width:'100%',
+      fontSize: '14px',
+     
+    },
+  }} position="top-right" /> 
+    <Routes>
+      
+      <Route element={<PublicLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register/>}/>
-      </Routes>
+        
+        <Route path="/bookAppointment" element={<BookAppointmentPage />} />
+        <Route path="/myAppointment" element={<AppointmentsPage />} />
+        <Route path="/healthTips" element={<HealthTipsPage />} />
+       
+      </Route> 
+      <Route path="/chat" element={<ChatPage/>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        element={
+          <ProtectedRoute>
+            <AdminLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route path="/dashboard/admin" element={<AdminDashboard/>} />
+        <Route path="/patients"  element={<PatientsDashboard/>}/>
+        <Route path="/doctors"  element={<MedcineDashboard/>}/>
+        <Route path="/admin/contact-history"  element={<ContactDashoard/>}/>
+        <Route path="/admin/RDV"  element={<RdvDashboard/>}/>
+        <Route path="/dashboard/doctor" element={<DoctorDashboard/>} />
+      </Route>
+    </Routes>
     </>
+    
   );
 }
 
 export default App;
-
