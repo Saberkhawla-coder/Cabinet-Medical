814b25d828e4a85d394425b75a6c8ace0abf2fd7
Saberkhawla-coder <khawla.saber06@gmail.com>
2025-12-28 07:34:27 +0100

    add code fillable

--- Back-end/project_medical/app/Models/Appointment.php
+++ Back-end/project_medical/app/Models/Appointment.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
+    protected $fillable = [
+    'doctor_id',
+    'patient_id',
+    'appointment_date',
+    'appointment_time',
+    'status'
+];
     public function doctor()
     {
         return $this->belongsTo(Doctor::class);
