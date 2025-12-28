fcadab895b2977bacf68edce717dd27603a96346
Saberkhawla-coder <khawla.saber06@gmail.com>
2025-12-28 20:31:18 +0100

    updated Appointment Models

--- Back-end/project_medical/app/Models/Appointment.php
+++ Back-end/project_medical/app/Models/Appointment.php
@@ -17,11 +17,11 @@ class Appointment extends Model
 ];
     public function doctor()
     {
-        return $this->belongsTo(Doctor::class);
+        return $this->belongsTo(Doctor::class, 'doctor_id'); 
     }
 
     public function patient()
     {
-        return $this->belongsTo(Patient::class);
+        return $this->belongsTo(Patient::class, 'patient_id'); 
     }
 }
